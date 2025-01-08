54fd4e76e289383ca2613a7bb4c0a80806dc0330
AIGQx <jalegalarza@gmail.com>
2025-01-08 00:34:57 -0500

    1/8/25 added buttons and fixes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin";
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -9,5 +11,12 @@ export default {
         prefix: "cat",
         defaultFlavour: "latte",
     }),
+	plugin(({ addUtilities }) => {
+		addUtilities({
+		".field-sizing-content": {
+		"field-sizing": "content",
+		},
+		});
+	}),
 	],
 }	
