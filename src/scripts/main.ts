154d43abf53247164045e0c55750f3df0bd830ef
Alejandro Galarza <jalegalarza@gmail.com>
2026-03-14 18:29:59 -0500

    ts

---
+++ src/scripts/main.ts
@@ -0,0 +1,4 @@
+const viewMain = document.getElementById('main') as HTMLElement;
+const viewProjects = document.getElementById('projects') as HTMLElement;
+const viewExperience = document.getElementById('experience') as HTMLElement;
+const viewContact = document.getElementById('contact') as HTMLElement;
